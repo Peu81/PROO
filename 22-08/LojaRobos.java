b44229e7ed240c71f70fc9abf263d669c09b4169
José Pedro <jpcd1@aluno.ifal.edu.br>
2025-08-22 21:22:27 -0300

    Create LojaRobos.java

---
+++ 22-08/LojaRobos.java
@@ -0,0 +1,14 @@
+public class LojaRobos {
+
+  public static void main(String[] args) {
+      Robo r1 = new Robo();
+      r1.andar();
+      system.out.println(r1.getVelocidadeRobo());
+
+      Robo r2 = new Robo();
+      r2.cmdAumentaVelocidade(12);
+    
+  }
+
+  
+}
